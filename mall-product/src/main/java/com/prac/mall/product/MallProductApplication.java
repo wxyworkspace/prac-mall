f5c7733f2d8655799bdb2e9bb9d2a4efb6c18c8b
wxyworkspace <wxyworkspace@gmail.com>
2022-10-23 21:10:00 +0800

    课时26完结，笔记03-5.2完结。nacos在腾讯云上部署，五个基本项目连接成功

--- mall-product/src/main/java/com/prac/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/prac/mall/product/MallProductApplication.java
@@ -3,7 +3,9 @@ package com.prac.mall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.prac.mall.product.dao")
 public class MallProductApplication {
